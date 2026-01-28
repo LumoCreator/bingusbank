f1b727bd1bff33611f3823f3cdcadc45225fdf08
LumoCreator <tvf2232@gmail.com>
2026-01-28 22:06:04 +0300

    Update qr.js

--- js/qr.js
+++ js/qr.js
@@ -1,1 +1,6 @@
-
+function makeQR(){
+  new QRCode(qr,{text:JSON.stringify({to:localStorage.card,amount:+amount.value})});
+}
+new Html5Qrcode("scan").start({facingMode:"environment"},{fps:10,qrbox:250},t=>{
+  const d=JSON.parse(t); localStorage.to=d.to; localStorage.amount=d.amount; location='send.html';
+});
